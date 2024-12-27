f97f08bb65a6288362e0ef7a48f4191467eb42f6
neojarvis <techadmin@iamneo.ai>
2024-12-27 05:57:25 +0000

    Auto-save-commit-12/27/2024--11:27:25-AM

--- testing/src/test/java/com/lti/AppTest.java
+++ testing/src/test/java/com/lti/AppTest.java
@@ -9,13 +9,18 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class AppTest 
 {
+  static WebDriver driver; 
   public static void main(String[] args) {
     
     try {
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
         driver.get("https://www.google.com");
     } catch (MalformedURLException e) {
         e.printStackTrace();
+    }
+    finally
+    {
+      driver.quit();
     }
   }
 }
