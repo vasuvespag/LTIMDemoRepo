f9cb6627b59b84c1838863b24ffc59c653c9a70b
neojarvis <techadmin@iamneo.ai>
2024-12-27 05:42:25 +0000

    Auto-save-commit-12/27/2024--11:12:25-AM

--- Sample/test/src/test/TestMyFirstSeleniumScript.java
+++ Sample/test/src/test/TestMyFirstSeleniumScript.java
@@ -1,11 +1,22 @@
-import org.openqa.Selenium;
+//import org.openqa.Selenium;
 import org.openqa.selenium.WebDriver;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.ChromeOptions;
 import org.openqa.selenium.RemoteWebDriver;
 
 public class TestMyFirstSeleniumScript {
 
     public static void main(String args[])
     {
-        
+        try
+        {
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"),new ChromeOptions());
+        }
+        catch(MalformedURLException e)
+        {
+            System.out.println("")
+        }
     }
 }
