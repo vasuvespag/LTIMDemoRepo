8bf8d217c1a2fbf1ed404a482ef663251ce9bd72
neojarvis <techadmin@iamneo.ai>
2024-12-30 11:06:54 +0000

    Auto-save-commit-12/30/2024--4:36:54-PM

--- firstday/src/test/java/com/example/TestWindows.java
+++ firstday/src/test/java/com/example/TestWindows.java
@@ -1,10 +1,14 @@
 package com.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.Set;
 
 public class TestWindows {
 
@@ -15,8 +19,35 @@ public class TestWindows {
         try{
             driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
             driver.get("https://demo.automationtesting.in/Windows.html");
-            
+            driver.findElement(By.xpath("(//button[@class='btn btn-info' and contains(text(),'click')])[1]")).click();
+            String currWindowHandle = driver.getWindowHandle();
 
+            Set<String> allWindowHandles = driver.getWindowHandles();
+
+            Iterator<String> itr = allWindowHandles.iterator();
+
+            while(itr.hasNext())
+            {
+                String childWindowHandle = itr.next();
+                if(!currWindowHandle.equalsIgnoreCase(childWindowHandle))
+                {
+                    driver.switchTo().window(childWindowHandle);
+                    String webdriverTxt = driver.findElement(By.xpath("(//div[@class='mb-4 h1'])[1]/following-sibling::p[2]")).getText();
+                    System.out.println(webdriverTxt);
+                }
+                driver.close();
+                driver.switchTo().window(currWindowHandle);
+            }
+
+            
+        }
+        catch(MalformedURLException me)
+        {
+            me.printStackTrace();
+        }
+        finally
+        {
+            driver.quit();
         }
     }
 
