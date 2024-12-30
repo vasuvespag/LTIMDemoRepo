e3bef7bd6fc962d4e73c65b755d3fb6ae1baded2
neojarvis <techadmin@iamneo.ai>
2024-12-30 09:21:32 +0000

    Auto-save-commit-12/30/2024--2:51:32-PM

--- firstday/src/test/java/com/example/TestSwitch.java
+++ firstday/src/test/java/com/example/TestSwitch.java
@@ -1,10 +1,12 @@
 package com.example;
 
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class TestSwitch {
@@ -15,15 +17,31 @@ public class TestSwitch {
             driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
             driver.get("https://demoqa.com/alerts");
             driver.findElement(By.xpath("//button[@id='alertButton']")).click();
+            Thread.sleep(3000);
             Alert alert1 = driver.switchTo().alert();
             alert1.accept();
+            Thread.sleep(3000);
 
-            driver.findElement(By.xpath("//button[@id='confirm']")).click();
-            Alert alert2 = 
+            driver.findElement(By.xpath("//button[@id='confirmButton']")).click();
+            Thread.sleep(3000);
+            Alert alert2 = driver.switchTo().alert();
+            Thread.sleep(3000);
+            alert2.dismiss();
+
+            
+            driver.findElement(By.xpath("//button[@id='promtButton']")).click();
+            Alert alert3 = driver.switchTo().alert();
+            Thread.sleep(3000);
+            alert3.sendKeys("Tharun");
+            Thread.sleep(3000);
+            alert3.accept();
         }
-        catch()
+    catch(MalformedURLException me)
         {
-
+            me.printStackTrace();
+        }
+        finally{
+            driver.quit();
         }
     }
 
