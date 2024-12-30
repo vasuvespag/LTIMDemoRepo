f11328efc70e68480287e62093812c520b56a456
neojarvis <techadmin@iamneo.ai>
2024-12-30 05:01:32 +0000

    Auto-save-commit-12/30/2024--10:31:32-AM

--- firstday/src/test/java/com/example/TestElems.java
+++ firstday/src/test/java/com/example/TestElems.java
@@ -1,18 +1,40 @@
 package com.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.chrome;;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class TestElems {
     public static WebDriver driver;
 
-    public static void main(String args[])
+    public static void main(String args[]) throws InterruptedException
     {
         try
         {
             driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
+            driver.get("https://demoqa.com/checkbox");
 
+            WebElement chkBox= driver.findElement(By.xpath("//span[@class='rct-title']"));
+
+            if(chkBox.isDisplayed())
+            {
+                chkBox.click();
+            }
+            
+            Thread.sleep(5000);
+
+        }
+        catch(MalformedURLException me)
+        {
+            me.printStackTrace();
+        }
+        finally{
+            driver.quit();
         }
     }
 
