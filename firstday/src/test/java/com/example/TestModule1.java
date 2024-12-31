6d69945cf7f19977994883280f515ca901a1cea2
neojarvis <techadmin@iamneo.ai>
2024-12-31 04:19:12 +0000

    Auto-save-commit-12/31/2024--9:49:12-AM

--- firstday/src/test/java/com/example/TestModule1.java
+++ firstday/src/test/java/com/example/TestModule1.java
@@ -1,11 +1,49 @@
 package com.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.MalformedInputException;
+
 public class TestModule1 {
     public WebDriver driver;
 
+    public TestModule1()
+    {
+        this.driver = null;
+    }
+
     public TestModule1(WebDriver driver)
     {
-        this
+        this.driver = driver;
+    }
+
+    
+
+    public WebDriver setDriver() throws MalformedURLException
+    {
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
+        return driver;
+    }
+
+    public void openPage(WebDriver driver)
+    {
+         
+        driver.get("https://demoqa.com/frames");
+        driver.switchTo().frame("frame1");
+    }
+    public static void main(String args[]) throws MalformedURLException
+    {
+        TestModule1 tm = new TestModule1();
+        tm.driver= tm.setDriver();
+        tm.openPage(tm.driver);
+        tm.printContent()
+        
+
+
     }
 
 }
