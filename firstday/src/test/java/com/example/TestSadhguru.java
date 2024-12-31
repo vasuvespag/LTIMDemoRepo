9c732c7b2af8e9a8ed5fedad304a937b1abafb49
neojarvis <techadmin@iamneo.ai>
2024-12-31 05:19:12 +0000

    Auto-save-commit-12/31/2024--10:49:12-AM

--- firstday/src/test/java/com/example/TestSadhguru.java
+++ firstday/src/test/java/com/example/TestSadhguru.java
@@ -4,9 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
@@ -14,11 +16,11 @@ public class TestSadhguru {
     public static WebDriver driver;
 
 @BeforeTest
-public void setUp()
+public void setUp() throws MalformedURLException
 {
     driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
     driver.manage().window().maximize();
-    driver.manage().timeouts().getPageLoadTimeout(Duration.ofSeconds(10));
+    //driver.manage().timeouts().getPageLoadTimeout(Duration.ofSeconds(10));
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     driver.get("https://adactinhotelapp.com");
 }
@@ -28,7 +30,16 @@ public void testHotelApp()
 {
     driver.findElement(By.id("username")).sendKeys("vasuvespag");
     driver.findElement(By.id("password")).sendKeys("Vasu1234");
-    driver.findElement(By.id())
+    driver.findElement(By.id("login")).click();
 }  
+
+@AfterClass
+public void tearDown()
+{
+    if(driver != null)
+    {
+        driver.quit();
+    }
+}
 
 }
