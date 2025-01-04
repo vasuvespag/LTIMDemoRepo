ac1be19be183c7cb1e18265057765118f1b6d749
neojarvis <techadmin@iamneo.ai>
2025-01-04 10:28:48 +0000

    Auto-save-commit-1/4/2025--3:58:49-PM

--- demoproject/src/test/java/com.utils/ScreenshotUtil.java
+++ demoproject/src/test/java/com.utils/ScreenshotUtil.java
@@ -1,10 +1,26 @@
 package com.utils;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.io.FileHandler;
+
 public class ScreenshotUtil {
 
-    public void captureScreenshot(String filePath)
+    public static void captureScreenshot(WebDriver driver,String filename)
     {
-        
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File file = ts.getScreenshotAs(OutputType.FILE);
+        String filepath=System.getProperty("user.dir")+"/screenshots/"+filename+".png";
+
+        try{
+            FileHandler.copy(file, new File(filepath));
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
