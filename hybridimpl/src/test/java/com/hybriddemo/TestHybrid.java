4880e8509aaa56c05041d3a8efe0176f59b884bf
neojarvis <techadmin@iamneo.ai>
2025-01-05 05:40:24 +0000

    Auto-save-commit-1/5/2025--11:10:24-AM

--- hybridimpl/src/test/java/com/hybriddemo/TestHybrid.java
+++ hybridimpl/src/test/java/com/hybriddemo/TestHybrid.java
@@ -1,6 +1,14 @@
 package com.hybriddemo;
 import org.testng.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 
@@ -8,6 +16,35 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class TestHybrid {
     public static WebDriver driver;
 
-    @Before
+    @BeforeMethod
+    public void setup()
+    {
+        String filePath = System.getProperty("user.dir");
+        System.out.println(filePath);
+        FileInputStream file = new FileInputStream(filePath+"/config/config.properties");
+        Properties prop = new Properties();
+        prop.load(file);
+        String url = prop.getProperty("Url");
+        ChromeOptions opt = new ChromeOptions();
+        opt.addArguments("--start-maximized");
+        opt.addArguments("--disable-notifications");
+        driver=new RemoteWebDriver(new URL("http://localhost:4444"), opt);
 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeOut(Duration.ofSeconds(15));
+
+        driver.get(url);
+    }
+
+    @Test
+    public void testHotel()
+    {
+        System.out.println("Started Driver");
+    }
+
+    @AfterMethod
+    public void tearDown()
+    {
+        driver.quit();
+    }
 }
