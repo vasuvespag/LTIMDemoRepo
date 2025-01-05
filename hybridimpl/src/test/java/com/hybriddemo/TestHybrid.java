441b4db7f9aedf697cd268a5bfce0c015e809bd8
neojarvis <techadmin@iamneo.ai>
2025-01-05 05:55:24 +0000

    Auto-save-commit-1/5/2025--11:25:24-AM

--- hybridimpl/src/test/java/com/hybriddemo/TestHybrid.java
+++ hybridimpl/src/test/java/com/hybriddemo/TestHybrid.java
@@ -4,8 +4,11 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
 import java.net.URL;
+import java.net.MalformedURLException;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,23 +18,24 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TestHybrid {
     public static WebDriver driver;
+    public static String url;
+    public static String filePath;
 
     @BeforeMethod
-    public void setup()
+    public void setup() throws MalformedURLException, IOException
     {
-        String filePath = System.getProperty("user.dir");
-        System.out.println(filePath);
+        filePath = System.getProperty("user.dir");
         FileInputStream file = new FileInputStream(filePath+"/config/config.properties");
         Properties prop = new Properties();
         prop.load(file);
-        String url = prop.getProperty("Url");
+        url = prop.getProperty("Url");
         ChromeOptions opt = new ChromeOptions();
         opt.addArguments("--start-maximized");
         opt.addArguments("--disable-notifications");
         driver=new RemoteWebDriver(new URL("http://localhost:4444"), opt);
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().timeouts().pageLoadTimeOut(Duration.ofSeconds(15));
+        //driver.manage().timeouts().pageLoadTimeOut(Duration.ofSeconds(15));
 
         driver.get(url);
     }
@@ -40,6 +44,9 @@ public class TestHybrid {
     public void testHotel()
     {
         System.out.println("Started Driver");
+        System.out.println(filePath);
+        System.out.println(url);
+        System.out.println(driver.getTitle());
     }
 
     @AfterMethod
