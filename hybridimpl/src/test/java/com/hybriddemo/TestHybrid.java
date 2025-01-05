71cb0512a71c28bd51e92f8c294c7c34b4ce749b
neojarvis <techadmin@iamneo.ai>
2025-01-05 05:25:24 +0000

    Auto-save-commit-1/5/2025--10:55:24-AM

---
+++ hybridimpl/src/test/java/com/hybriddemo/TestHybrid.java
@@ -0,0 +1,13 @@
+package com.hybriddemo;
+import org.testng.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+
+
+public class TestHybrid {
+    public static WebDriver driver;
+
+    @Before
+
+}
