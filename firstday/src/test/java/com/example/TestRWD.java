59fb32691272ef2262a61b7074f62b8dbe94e681
neojarvis <techadmin@iamneo.ai>
2025-01-13 10:35:20 +0000

    Auto-save-commit-1/13/2025--4:05:20-PM

--- firstday/src/test/java/com/example/TestRWD.java
+++ firstday/src/test/java/com/example/TestRWD.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -16,10 +19,34 @@ public class TestRWD {
     @BeforeTest
     public void setup() throws MalformedURLException
     {
-        driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/"), new ChromeOptions());
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://adactinhotelapp.com");
-        driver.manage().t
+        driver.get("https://www.adactinhotelapp.com");
+        driver.manage().window().maximize();
     }
 
+    @Test
+    public void testResponsiveness() throws InterruptedException
+    {
+        //for desktop resolution
+        driver.manage().window().setSize(new Dimension(1080,800));
+        Thread.sleep(5000);
+
+        //for tab resolution
+        driver.manage().window().setSize(new Dimension(900,800));
+        Thread.sleep(5000);
+
+        //for mobile resolution
+        driver.manage().window().setSize(new Dimension(360,800));
+        Thread.sleep(5000);
+    }
+
+    @AfterTest
+    public void tearDown()
+    {
+        if(driver != null)
+        {
+            driver.quit();
+        }
+    }
 }
